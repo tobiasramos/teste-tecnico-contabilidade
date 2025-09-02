492b08d0617aeb143434fa64179b6ea9c7603dba
Tobias <tobiasramossilva@gmail.com>
2025-09-02 08:28:04 -0300

    Criando o repositório do teste técnico

---
+++ app/layout.tsx
@@ -0,0 +1,17 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Contabilidade.com",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html>
+      <body>{children}</body>
+    </html>
+  );
+}
